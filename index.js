1d8eeaa6f7530016786669f54eea5480f2764765
Ian Scott Martin <ianscottmartin@gmail.com>
2023-04-03 22:14:45 -0600

    finit

--- index.js
+++ index.js
@@ -2,5 +2,8 @@
 document.querySelector("main#main").remove();
 const newHeader = document.createElement("h1");
 newHeader.id = "victory"
-newHeader.innerHTML = "Scott Martin is the champion!";
+// newHeader.innerHTML = "Scott Martin is the champion!";
+newHeader.textContent = "Scott Martin is the champion";
 newHeader.className = "victory";
+
+
